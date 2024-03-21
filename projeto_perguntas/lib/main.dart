2a50bbaecb1358bb16aef3b79137ca6de5b4259d
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2024-03-21 15:09:10 -0300

    inserido botao com textos

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -7,12 +7,23 @@ void main(List<String> args) {
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas"),
         ),
-        body: Text("Ola Flutter!!!"),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas.elementAt(0)),
+            ElevatedButton(onPressed: null, child: Text("Resposta 1")),
+            ElevatedButton(onPressed: null, child: Text("Resposta 2")),
+            ElevatedButton(onPressed: null, child: Text("Resposta 3")),
+          ],
+        ),
       ),
     );
   }
