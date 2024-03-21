467134d11b56fde6484ba8ef271ffea568d847e9
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2024-03-21 11:15:54 -0300

    alguns exemplos de funcoes

--- exercicios_dart/exercicio_funcoes.dart
+++ exercicios_dart/exercicio_funcoes.dart
@@ -1,3 +1,33 @@
+// funcao com tipo dynamic sem retorno
+soma(a, b) {
+  print(a + b);
+}
+
+// funcao void tipada
+void soma3(int a, int b) {
+  print(a + b);
+}
+
+//funcao tipada
+int soma2(int a, int b) {
+  return 2 + b;
+}
+
+// funcao com funcao como parametro
+int exec(int a, int b, int Function(int, int) fn) {
+  return fn(a, b);
+}
+
 void main(List<String> args) {
-  
+  soma(2, 3);
+  final int r = soma2(3, 7);
+  print("O Valor da some é: $r");
+  soma3(4, 2);
+  final s = exec(2, 3, (a, b) {
+    return a - b;
+  });
+  print("o resultado é $s !!!!");
+
+  final t = exec(4, 5, (a, b) => a * b + 100);
+  print("O resultado é $t !!!!");
 }
