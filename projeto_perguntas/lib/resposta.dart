73c88bd366bb7e957ca03a24beda3a60a763ff28
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2024-03-25 08:48:46 -0300

    repafotacao da classe resposta

--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-  const Resposta(this.texto);
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return       const ElevatedButton(
-                onPressed: null, child: Text("Resposta 1"));
-    
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue, 
+            foregroundColor: Colors.white),
+        onPressed: quandoSelecionado,
+        child: const Text("Resposta 1"),
+      ),
+    );
   }
 }
