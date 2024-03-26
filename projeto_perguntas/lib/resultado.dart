0f95c4cfcb50487cb70c03d00c6da9c970ed695d
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2024-03-26 13:35:22 -0300

    refatoracao

--- projeto_perguntas/lib/resultado.dart
+++ projeto_perguntas/lib/resultado.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  
+  final int pontuacao;
+
+  const Resultado(this.pontuacao);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return "Parabens!!!!";
+    } else if (pontuacao < 12) {
+      return "Voce e Bom!!!";
+    } else if (pontuacao < 16) {
+      return "Impressionante!!!!";
+    } else {
+      return "Nivel Jedi!!!";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Text(
-        "Parabens!!!",
+        fraseResultado,
         style: TextStyle(fontSize: 28),
       ),
     );
